b6d94f75ee64e4f91f270dcc66aa1a4010a3dd57
Ivison Filho <ivisoncaufilho@gmail.com>
2025-08-10 11:48:13 -0300

    Update vite.config.ts
    Teste deploy

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/ieeecsufrn/",
   plugins: [
     react(),
     mode === 'development' &&
@@ -19,5 +20,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/ieeecsufrn",
 }));
